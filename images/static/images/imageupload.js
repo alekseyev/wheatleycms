62055da1f53a911c64186c22b2a71e20fd679f93
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2012-06-28 18:01:20 +0300

    Image insert widget

--- images/static/images/imageupload.js
+++ images/static/images/imageupload.js
@@ -1,3 +1,5 @@
+image_data = {}
+
 $(document).ready(function(){
 })
 
@@ -8,10 +10,33 @@ function start_upload(url) {
     })
     $('#upload-progress').show()
     $('#upload-submit').attr('disabled', 'disabled')
-    return false
 }
 
 function finish_upload(data, textStatus, jqXHR) {
     $('#upload-form').modal('hide')
-    wysihtml_editor.composer.commands.exec('insertImage', { src: data.url })
+    $('#upload-progress').hide()
+    $('#id_image_inner').val('')
+    $('#upload-submit').removeAttr('disabled')
+    image_data = data
+    show_image()
+}
+
+function show_image() {
+    $('#image-modal-thumbnail').attr('src', image_data.thumbnail_url)
+    $('#image-modal').modal('show')
+}
+
+function insert_image() {
+    wysihtml_editor.composer.commands.exec(
+        'insertHTML', 
+        '<img src="' + image_data.url + '"></a>'
+        )
+    $('#image-modal').modal('hide')
+}
+
+function insert_thumbnail() {
+    wysihtml_editor.composer.commands.exec(
+        'insertHTML', 
+        '<a href="' + image_data.url + '"><img src="' + image_data.thumbnail_url + '"></a>')
+    $('#image-modal').modal('hide')
 }
