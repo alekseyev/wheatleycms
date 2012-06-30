f06c8a4db0dfe4de1a4fa4eb42206369cf8ea2f4
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2012-06-30 23:11:12 +0300

    Extended parserRules for wysihtml5 object (in English: allows more tags to be used)

--- wysihtml5/static/wysihtml5/init-wysihtml5.js
+++ wysihtml5/static/wysihtml5/init-wysihtml5.js
@@ -7,6 +7,39 @@ $(function(){
         "lists": true,
         "html": true, 
         "link": true, 
-        "image": true 
+        "image": true,
+        parserRules: {
+            tags: {
+                "b":  {},
+                "i":  {},
+                "br": {},
+                "ol": {},
+                "ul": {},
+                "li": {},
+                "h1": {},
+                "h2": {},
+                "p": {},
+                "blockquote": {},
+                "div": {},
+                "u": 1,
+                "img": {
+                    "check_attributes": {
+                        "width": "numbers",
+                        "alt": "alt",
+                        "src": "url",
+                        "height": "numbers"
+                    }
+                },
+                "a":  {
+                    set_attributes: {
+                        target: "_blank",
+                        rel:    "nofollow"
+                    },
+                    check_attributes: {
+                        href:   "url" // important to avoid XSS
+                    }
+                }
+            }
+        }
     }).data('wysihtml5').editor
 })
